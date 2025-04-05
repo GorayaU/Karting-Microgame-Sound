a20c73df64d181e56eaa6b92e8b679df642541b0
GorayaU <144922343+GorayaU@users.noreply.github.com>
2025-04-05 16:21:35 -0400

    1

--- Assets/Karting/Scripts/Audio Shit/FmodEvents.cs
+++ Assets/Karting/Scripts/Audio Shit/FmodEvents.cs
@@ -5,19 +5,27 @@ namespace Karting.Audio_Shit
 {
     public class FmodEvents : MonoBehaviour
     {
-        [field: Header("Music")]
+        [field: Header("Ambiance")]
         [field: SerializeField] public EventReference Music { get; private set; }
+        [field: SerializeField] public EventReference Clapping { get; private set; }
+        [field: SerializeField] public EventReference Crows { get; private set; }
+        [field: SerializeField] public EventReference Wind { get; private set; }
         
-        [field: Header("Character Noises")]
+        [field: Header("Car Sounds")]
         [field: SerializeField] public EventReference Engine { get; private set; }
         [field: SerializeField] public EventReference Brake { get; private set; }
+        [field: SerializeField] public EventReference Reverse { get; private set; }
         
-        [field: Header("Injury Sounds")]
+        [field: Header("Misc")]
         [field: SerializeField] public EventReference WallHit { get; private set; }
+        [field: SerializeField] public EventReference CheckpointPassed { get; private set; }
+        [field: SerializeField] public EventReference Congrats { get; private set; }
+        [field: SerializeField] public EventReference Lights { get; private set; }
+        [field: SerializeField] public EventReference Winning { get; private set; }
+        
         
         public static FmodEvents Instance { get; private set; }
         
-
         private void Awake()
         {
             Instance = this;
